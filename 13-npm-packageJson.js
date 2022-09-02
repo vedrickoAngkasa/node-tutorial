7da808c94ee052401b85a36d1237c47291dd97f4
vedricko <vedrickoangkasa19@gmail.com>
2022-09-02 16:35:00 +0800

    second commit

---
+++ 13-npm-packageJson.js
@@ -0,0 +1,20 @@
+// npm global command, comes with node
+// npm--version
+
+// local dependency - use it only in this particular project
+// npm i <packageName>
+
+// global dependency - use it in any project
+// npm install -g <packageName>
+// sudo npm install -g <packageName> (mac)
+
+// package.json manifest file (stores important info about project/package)'
+// manual approach (create package. j son in the root, create properties etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default)
+
+// package.json
+    // to use start we need to write only npm start, there are some command that doesn't use npm run
+    // to use dev we need to write npm run dev
+
+    console.log("hello world");
